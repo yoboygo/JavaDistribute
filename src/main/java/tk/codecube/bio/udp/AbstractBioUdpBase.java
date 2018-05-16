422499d407eb040ca61a6ba7e7a091fdd3338e9f
songjianlong <songjianlong@idengyun.com>
2018-05-16 15:30:41 +0800

    udp

--- src/main/java/tk/codecube/bio/udp/AbstractBioUdpBase.java
+++ src/main/java/tk/codecube/bio/udp/AbstractBioUdpBase.java
@@ -3,5 +3,9 @@ package tk.codecube.bio.udp;
 public class AbstractBioUdpBase {
 
 	protected static final String SERVER_IP = "127.0.0.1";
-	protected static final int SERVER_PORT = 999;
+	
+	protected static final int SOCKET_PORT = 999;
+	
+	protected static final int SERVER_1_PORT = 1000;
+	protected static final int SERVER_2_PORT = 1001;
 }
--- src/main/java/tk/codecube/bio/udp/BioUdpService.java
+++ src/main/java/tk/codecube/bio/udp/BioUdpService.java
@@ -16,22 +16,22 @@ public class BioUdpService extends AbstractBioUdpBase{
 
 	public static void main(String[] args) throws IOException {
 		byte[] buffer = new byte[1024];
-		String data = "Hello！+ 1";
+		String data = "udpClient2";
 		//由于UDP/IP是无连接的，如果希望双向通信，就必须启动一个监听端口，承担服务器的职责，如果不能
 		//绑定到指定端口，则抛出SocketException
-		InetSocketAddress socketAddress = new InetSocketAddress(SERVER_IP, SERVER_PORT + 1);
-		DatagramSocket serverSocket = new DatagramSocket(SERVER_PORT + 1);
+		InetSocketAddress socketAddress = new InetSocketAddress(SERVER_IP, SERVER_1_PORT);
+		DatagramSocket serverSocket = new DatagramSocket(SERVER_2_PORT);
 		DatagramPacket receivePacket = new DatagramPacket(buffer,buffer.length);
 		DatagramSocket socket = new DatagramSocket();
-		DatagramPacket packet = new DatagramPacket(data.getBytes(), data.length(),socketAddress);
+		DatagramPacket packet = new DatagramPacket(data.getBytes(Charset.forName("UTF-8")), data.length(),socketAddress);
 		//阻塞发送packet到指定的服务器和端口，当出现网络IO异常时抛出IOException,当连不上目标地址和端口时，抛出PortUnreachableException
-		System.out.println("+1发送信息：" + data);
+		System.out.println("发送信息：" + data);
 		socket.send(packet);
 		//阻塞并同步读取流信息，如接收到的流信息比packet长度长,则删除更长的信息，可通过调用setSoTimeout
 		//设置读取流的超时时间
 		socket.setSoTimeout(Integer.MAX_VALUE);
 		serverSocket.receive(receivePacket);
-		System.out.println("+1接收到信息：" + new String(buffer,Charset.defaultCharset()));
+		System.out.println("接收到信息：" + new String(buffer,Charset.forName("UTF-8")));
 	}
 
 }
